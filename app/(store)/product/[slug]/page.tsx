b76be50e77ded6ee4ce6cf40f062435fa082c12e
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-10 15:41:41 -0300

    product page init

---
+++ app/(store)/product/[slug]/page.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image"
+
+export default function ProductPage() {
+	return (
+		<div className="relative grid max-h-[860px] grid-cols-3">
+			<div className="col-span-2 overflow-hidden">
+				<Image
+					src={"/moletom-ai-side.png"}
+					alt={"product.title"}
+					width={1000}
+					height={1000}
+					className="object-cover w-full h-full"
+				/>
+			</div>
+			<div className="flex flex-col justify-center px-12">
+				<h1 className="text-3xl font-bold leading-tight">Product Title</h1>
+				<p className="mt-2 leading-relaxed text-zinc-400">
+					Product description goes here. This is a placeholder for the product
+					details.
+				</p>
+
+				<div className="mt-8 flex items-center gap-3">
+					<span className="inline rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+						R$ 284
+					</span>
+					<span className="text-sx text-zinc-400">Em até 12x de R$ 23,67</span>
+				</div>
+
+				<div className="mt-8 space-y-4">
+					<span className="block font-semibold">Tamanhos</span>
+					<div className="flex gap-2">
+						<button
+							type="button"
+							className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+						>
+							P
+						</button>
+						<button
+							type="button"
+							className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+						>
+							M
+						</button>
+						<button
+							type="button"
+							className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+						>
+							G
+						</button>
+						<button
+							type="button"
+							className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+						>
+							GG
+						</button>
+					</div>
+				</div>
+
+				<button
+					type="button"
+					className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
+				>
+					Adicionar ao carrinho
+				</button>
+			</div>
+		</div>
+	)
+}
