567a3baf5f59907899fd1680bb6ba37f920fb690
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-10 14:10:29 -0300

    api instance, env schema

---
+++ env.ts
@@ -0,0 +1,15 @@
+import z from "zod"
+
+const envSchema = z.object({
+	NEXT_PUBLIC_API_URL: z.url(),
+})
+
+const { data, success, error } = envSchema.safeParse(process.env)
+
+if (!success) {
+	console.error("Invalid environment variables", z.treeifyError(error))
+
+	throw new Error("Invalid environment variables")
+}
+
+export const env = data
